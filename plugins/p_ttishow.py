4c303e7c0ae5564350f9d25b837de23bfc472679
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-01-12 14:07:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,14 +37,18 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('Updates', url='https://t.me/Justaupdatechannel')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
             text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
             reply_markup=reply_markup)
     else:
+        for u in message.new_chat_members:
+            zaute = [[
+            InlineKeyboardButton('👉 🎥 Press me... 🎥 👈', url="https://t.me/cinemaenglish123")
+        ]]
         if MELCOW_NEW_USERS:
             for u in message.new_chat_members:
                 if (temp.MELCOW).get('welcome') is not None:
